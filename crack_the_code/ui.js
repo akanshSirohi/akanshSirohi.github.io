ab9d3ee47ff9f0088237557baf2ab45916e2015a
Akansh Sirohi <34837239+akanshSirohi@users.noreply.github.com>
2020-12-05 15:38:21 +0530

    Add files via upload

---
+++ crack_the_code/ui.js
@@ -0,0 +1,12 @@
+const closeSuccess = () => {
+  $("#success").hide();
+};
+const showSuccess = () => {
+  $("#success").show();
+};
+const closeFail = () => {
+  $("#fail").hide();
+};
+const showFail = () => {
+  $("#fail").show();
+};
